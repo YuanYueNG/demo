1ba7bdba57c1fba3baeb90b75896ec9fe7eceee2
YuanYueNG <niangao1103@gmail.com>
2020-04-12 16:52:04 +0800

    HelloWorld2

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("343d");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("He3423423");
+	}
+}
